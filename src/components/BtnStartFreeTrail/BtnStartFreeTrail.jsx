1af7060a192e11f8fd5188a5b8b7d619d4dafa47
umairakramspsi <136310241+umairakramspsi@users.noreply.github.com>
2024-07-24 11:59:04 +0500

    btnfreetrail

---
+++ src/components/BtnStartFreeTrail/BtnStartFreeTrail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BtnStartFreeTrail = () => {
+  return (
+    <>
+    <button className='uppercase bg-[rgb(80,37,255)] text-white rounded-[10px] text-[23px] drop-shadow-[0px_6px_25px_rgb(51,0,255)] py-6 px-9 font-bold hover:scale-105 hover:bg-[rgb(95,60,234)]'>Start Free Trial
+    </button>
+    </>
+  )
+}
+
+export default BtnStartFreeTrail
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "../components/Home/Home.css"
+import BtnStartFreeTrail from '../components/BtnStartFreeTrail/BtnStartFreeTrail'
 
 const Home = () => {
   return (
-    <div> Home </div>
+    <div> Home
+      
+     
+       </div>
   )
 }
 
